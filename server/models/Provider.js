5b2964621f3fbcfd24c9287fd8b36d9f289831c7
Thomas Swann <tkswann@mac.com>
2016-10-20 13:59:32 -0500

    Provider model added

---
+++ server/models/Provider.js
@@ -0,0 +1,46 @@
+'use strict'
+const mongoose = require('mongoose')
+
+const providerSchema = mongoose.Schema({
+  // userId pulled from User collection
+  userId: {
+    type: String,
+    required: true,
+  },
+  categories: {
+    type: Array,
+    required: true,
+    default: []
+  },
+  skills: {
+    type: Array,
+    required: true,
+    default: []
+  },
+  experience: {
+    type: String,
+    required: true,
+  },
+  rate: {
+    type: String,
+    required: true,
+  },
+  flexible: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  bio: {
+    type: String,
+    required: true,
+  },
+  available: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+})
+
+
+
+module.exports = mongoose.model('Provider', providerSchema)
