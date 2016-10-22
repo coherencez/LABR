3ba78df10aa8f22a52c3104f49fb8b0bd82ae3e9
Thomas Swann <tkswann@mac.com>
2016-10-22 18:17:50 -0500

    added react-native-easy-grid, basic provider view layout complete

--- js/containers/Providers.js
+++ js/containers/Providers.js
@@ -1,22 +1,129 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet } from 'react-native';
-import { Button, Text, Container, Content, Header, Footer } from 'native-base'
+import { AppRegistry, StyleSheet, Image } from 'react-native'
+import { Col, Row, Grid } from 'react-native-easy-grid'
+import { Container, Content, Card, CardItem, Thumbnail, Text, Button, List, ListItem } from 'native-base';
+
 
 export default class Providers extends Component {
   render() {
     return (
         <Container style={styles.alignmentFix}>
-          <Header>
-            <Text> Providers </Text>
-          </Header>
           <Content>
-            <Text>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Text>
+          <Grid>
+              <Col style={{ backgroundColor: '#D954D7'}}>
+                <List>
+                  <ListItem>
+                    <Card>
+                      <CardItem>
+                        <Text>Nashville</Text>
+                      </CardItem>
+                      <CardItem cardBody>
+                        <Image source={{uri: 'https://c2.staticflickr.com/6/5509/12298744374_9441f9cbeb_b.jpg'}} />
+                        <Button transparent textStyle={{color: '#87838B'}} onPress={this.handleNashville}>
+                            389 Providers Online
+                        </Button>
+                      </CardItem>
+                    </Card>
+                  </ListItem>
+                  <ListItem>
+                    <Card>
+                      <CardItem>
+                        <Text>Las Vegas</Text>
+                      </CardItem>
+                      <CardItem cardBody>
+                        <Image source={{uri: 'https://www.firstoptiononline.com/wp-content/uploads/2014/10/las-vegas-skyline.jpg'}} />
+                        <Button transparent textStyle={{color: '#87838B'}} onPress={this.handleNashville}>
+                            452 Providers Online
+                        </Button>
+                      </CardItem>
+                    </Card>
+                  </ListItem>
+                  <ListItem>
+                    <Card>
+                      <CardItem>
+                        <Text>Las Vegas</Text>
+                      </CardItem>
+                      <CardItem cardBody>
+                        <Image source={{uri: 'https://www.firstoptiononline.com/wp-content/uploads/2014/10/las-vegas-skyline.jpg'}} />
+                        <Button transparent textStyle={{color: '#87838B'}} onPress={this.handleNashville}>
+                            452 Providers Online
+                        </Button>
+                      </CardItem>
+                    </Card>
+                  </ListItem>
+                  <ListItem>
+                    <Card>
+                      <CardItem>
+                        <Text>Las Vegas</Text>
+                      </CardItem>
+                      <CardItem cardBody>
+                        <Image source={{uri: 'https://www.firstoptiononline.com/wp-content/uploads/2014/10/las-vegas-skyline.jpg'}} />
+                        <Button transparent textStyle={{color: '#87838B'}} onPress={this.handleNashville}>
+                            452 Providers Online
+                        </Button>
+                      </CardItem>
+                    </Card>
+                  </ListItem>
+                </List>
+              </Col>
+              <Col style={{ backgroundColor: '#D93735' }}>
+                <List>
+                  <ListItem>
+                    <Card>
+                      <CardItem>
+                        <Text>Nashville</Text>
+                      </CardItem>
+                      <CardItem cardBody>
+                        <Image source={{uri: 'https://c2.staticflickr.com/6/5509/12298744374_9441f9cbeb_b.jpg'}} />
+                        <Button transparent textStyle={{color: '#87838B'}} onPress={this.handleNashville}>
+                            389 Providers Online
+                        </Button>
+                      </CardItem>
+                    </Card>
+                  </ListItem>
+                  <ListItem>
+                    <Card>
+                      <CardItem>
+                        <Text>Nashville</Text>
+                      </CardItem>
+                      <CardItem cardBody>
+                        <Image source={{uri: 'https://c2.staticflickr.com/6/5509/12298744374_9441f9cbeb_b.jpg'}} />
+                        <Button transparent textStyle={{color: '#87838B'}} onPress={this.handleNashville}>
+                            389 Providers Online
+                        </Button>
+                      </CardItem>
+                    </Card>
+                  </ListItem>
+                  <ListItem>
+                    <Card>
+                      <CardItem>
+                        <Text>Nashville</Text>
+                      </CardItem>
+                      <CardItem cardBody>
+                        <Image source={{uri: 'https://c2.staticflickr.com/6/5509/12298744374_9441f9cbeb_b.jpg'}} />
+                        <Button transparent textStyle={{color: '#87838B'}} onPress={this.handleNashville}>
+                            389 Providers Online
+                        </Button>
+                      </CardItem>
+                    </Card>
+                  </ListItem>
+                  <ListItem>
+                    <Card>
+                      <CardItem>
+                        <Text>Las Vegas</Text>
+                      </CardItem>
+                      <CardItem cardBody>
+                        <Image source={{uri: 'https://www.firstoptiononline.com/wp-content/uploads/2014/10/las-vegas-skyline.jpg'}} />
+                        <Button transparent textStyle={{color: '#87838B'}} onPress={this.handleNashville}>
+                            452 Providers Online
+                        </Button>
+                      </CardItem>
+                    </Card>
+                  </ListItem>
+                </List>
+              </Col>
+          </Grid>
           </Content>
-          <Footer>
-
-          </Footer>
         </Container>
     );
   }
