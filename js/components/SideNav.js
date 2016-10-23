ab967b99601a8a989191f93468609056f8b7bd2a
Thomas Swann <tkswann@mac.com>
2016-10-22 22:19:41 -0500

    fleshed out sideMenu

--- js/components/SideNav.js
+++ js/components/SideNav.js
@@ -1,39 +1,41 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  View
-} from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Container, Content, List, ListItem, Text, Icon, Badge } from 'native-base';
-export default class App extends Component {
+
+
+export default class SideNav extends Component {
   render() {
     return (
       <Container>
         <Content>
             <List style={styles.alignmentFix}>
+              <ListItem iconLeft>
+                <Icon name='md-contact' />
+                <Text>Sign Up</Text>
+                <Text note>Note here</Text>
+              </ListItem>
                 <ListItem iconLeft>
-                    <Icon name='ios-chatboxes' />
-                    <Text>Simon Mignolet</Text>
-                </ListItem>
-                <ListItem iconLeft>
-                    <Icon name='ios-alarm' />
-                    <Text>Nathaniel Clyne</Text>
-                    <Badge>2</Badge>
-                </ListItem>
-                <ListItem iconLeft>
-                    <Icon name='ios-notifications' />
-                    <Text>Dejan Lovren</Text>
-                    <Text note>Note here</Text>
+                    <Icon name='md-archive' />
+                    <Text>History</Text>
                 </ListItem>
                 <ListItem iconLeft iconRight>
-                    <Icon name='ios-mic' />
-                    <Text>Mama Sakho</Text>
-                    <Icon name='ios-mic-outline' />
+                    <Icon name='ios-briefcase' />
+                    <Text>Business Account</Text>
+                    <Icon name='ios-mic-outline' style={styles.hidden}/>
+                </ListItem>
+                <ListItem iconLeft>
+                    <Icon name='md-text' />
+                    <Text>Messages</Text>
+                    <Badge>3</Badge>
+                </ListItem>
+                <ListItem iconLeft>
+                    <Icon name='md-aperture' />
+                    <Text>Settings</Text>
                 </ListItem>
             </List>
         </Content>
     </Container>
-    );
+    )
   }
 }
 
@@ -41,4 +43,7 @@ const styles = StyleSheet.create({
   alignmentFix: {
     marginTop: 60,
   },
+  hidden: {
+    opacity: 0,
+  }
 });
