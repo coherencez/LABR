9b422e2997cf58195e1f42ad09dc68dc87c71441
Thomas Swann <tkswann@mac.com>
2016-10-27 19:03:32 -0500

    bio rendering dynamically

--- js/components/BusinessProfile.js
+++ js/components/BusinessProfile.js
@@ -70,7 +70,7 @@ export default class BusinessProfile extends Component {
               </Col>
               <Col style={{ width: 200, padding: 5 }}>
                 <Text style={styles.helperText}>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  {this.props.provider.bio}
                 </Text>
 
               </Col>
