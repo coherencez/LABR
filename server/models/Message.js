c3b5fe12afad6b1ac673cf37a63253fc2712e7dc
Thomas Swann <tkswann@mac.com>
2016-11-02 23:50:59 -0500

    changed Message model to be an Array containg Objects with all message data

--- server/models/Message.js
+++ server/models/Message.js
@@ -2,30 +2,31 @@
 const mongoose = require('mongoose')
 
 const messageSchema = mongoose.Schema({
-  _id: {
-    type: String,
-    required: true,
-  },
-  text: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: String,
-    required: true,
-  },
-  user: {
-    type: Object,
+  messages: [{
     _id: {
       type: String,
       required: true,
     },
-    name: {
+    text: {
       type: String,
       required: true,
-    }
-
-  }
+    },
+    createdAt: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Object,
+      _id: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      }
+    },
+  }],
 })
 
 
