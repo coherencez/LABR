1dbbe6493a3b5a612b9b150fbe2fca97f0c98d0e
Thomas Swann <tkswann@mac.com>
2016-11-03 23:25:42 -0500

    updated welcome paragraph

--- js/containers/App.js
+++ js/containers/App.js
@@ -31,7 +31,11 @@ export default class App extends Component {
           </Header>
           <Content>
             <Text style={styles.instructions}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              This is LABR. Your one stop shop to getting everyday tasks completed on-demand.
+              Find providers that are online, and available in your area today! Just press the
+              locations button below to get started viewing providers. Or you can swipe right
+              to open the menu and signup for an account. After that, think about becoming a
+              provider yourself!
             </Text>
 
 
@@ -67,8 +71,9 @@ const styles = StyleSheet.create({
   instructions: {
     textAlign: 'center',
     color: fontColorWhite,
-    fontSize: 10,
-    fontFamily: fontFamily
+    fontSize: 15,
+    fontFamily: fontFamily,
+    marginTop: 25,
   },
   button: {
     backgroundColor: buttonBgColor,
